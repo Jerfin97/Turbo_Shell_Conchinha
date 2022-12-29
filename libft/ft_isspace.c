ee8229779f167135fe9effe9b45fbd04e56443bf
jeluiz4 <jeffluiz97@gmail.com>
2022-12-29 08:52:26 -0300

    Added_Isspace

--- libft/ft_isspace.c
+++ libft/ft_isspace.c
@@ -1,40 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeluiz4 <jeffluiz97@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/09 13:24:53 by jeluiz4           #+#    #+#             */
-/*   Updated: 2022/12/29 08:37:03 by jeluiz4          ###   ########.fr       */
+/*   Created: 2022/12/29 08:28:33 by jeluiz4           #+#    #+#             */
+/*   Updated: 2022/12/29 08:29:51 by jeluiz4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+int	ft_isspace(char c)
 {
-	int	out;
-	int	sign;
-	int	i;
-
-	sign = 1;
-	out = 0;
-	i = 0;
-	while (ft_isspace(str[i]))
-	{
-		i++;
-	}
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = sign * -1;
-		i++;
-	}
-	while (ft_isdigit(str[i]))
-	{
-		out = out * 10 + str[i] - '0';
-		i++;
-	}
-	return (out * sign);
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v')
+		return (1);
+	return (0);
 }
