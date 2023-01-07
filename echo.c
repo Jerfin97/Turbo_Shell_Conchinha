2d61f9f592808f4cb125ba246260f8cb830a87e6
jeluiz4 <jeffluiz97@gmail.com>
2023-01-07 19:09:31 -0300

    Solved_Bug_-n_In_Echo

--- echo.c
+++ echo.c
@@ -6,11 +6,32 @@
 /*   By: jeluiz4 <jeffluiz97@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 22:24:52 by jeluiz4           #+#    #+#             */
-/*   Updated: 2023/01/07 10:49:53 by jeluiz4          ###   ########.fr       */
+/*   Updated: 2023/01/07 19:08:21 by jeluiz4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_mini.h"
+
+int	ft_find_n(char **args, int i)
+{
+	int	j;
+
+	j = 0;
+	if (args[i][j++] == '-')
+	{
+		while ((args[i][j] == 'n'))
+			j++;
+		if (args[i][j] == 'n' || args[i][j] == '\0')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
 
 void	ft_multi_join(t_input *inp, int i, char **args)
 {
@@ -32,41 +53,27 @@ void	ft_multi_join(t_input *inp, int i, char **args)
 	free(swp);
 }
 
-int	ft_find_n(char **args)
+void	ft_clean_echo(t_shell *blk, t_input *inp, char **args)
 {
 	int	i;
 
-	i = 1;
-	if (args[1][0] == '-')
+	i = 2;
+	if (args[1] != NULL)
 	{
-		while ((args[1][i] == 'n'))
-			i++;
-		if (args[1][i] == 'n' || args[1][i] == '\0')
+		if (ft_find_n(args, 1))
 		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return (0);
-}
-
-void	ft_clean_echo(t_shell *blk, t_input *inp, char **args)
-{
-	if (args[2] != NULL)
-	{
-		if (ft_find_n(args))
-		{
-			ft_multi_join(inp, 2, args);
+			while (ft_find_n(args, i))
+				i++;
+			ft_multi_join(inp, i, args);
 			inp->size = -1;
 		}
 		else
 			ft_multi_join(inp, 1, args);
 	}
-	else
+	else if (args[1])
 		inp->echo_print = ft_strjoin(args[1], "\0");
+	else
+		inp->echo_print = ft_strdup("");
 	blk->rs = 0;
 }
 
@@ -75,13 +82,11 @@ void	ft_echo(t_shell *blk, t_input *inp, char **args)
 	ft_clean_echo(blk, inp, args);
 	if (inp->size == -1)
 	{
-		//ft_putstr_fd(inp->echo_print, 1);
 		printf("%s", inp->echo_print);
 	}
 	else
 	{
 		printf("%s\n", inp->echo_print);
-		//ft_putendl_fd(inp->echo_print, 1);
 	}
 	free(inp->echo_print);
 	blk->rs = 0;
