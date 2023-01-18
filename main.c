90d753b1ee3875640ee027505d3d2be8580b4f79
Daniel Vargas <daniel_v93@hotmail.com>
2023-01-17 22:16:07 -0300

    Redirect test in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jeluiz4 <jeffluiz97@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 10:17:32 by jeluiz4           #+#    #+#             */
-/*   Updated: 2023/01/11 16:03:39 by jeluiz4          ###   ########.fr       */
+/*   Updated: 2023/01/17 22:12:49 by dvargas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,30 @@ int	main(int argc, char **argv, char **envp)
 	signal(SIGINT, signal_handler);
 	signal(SIGQUIT, signal_handler);
 	//ft_access(blk, inp);
-	ft_prompt(blk, inp);
+	char *teste = "< file.txt grep file";
+	printf("Deveria ser 1 Resultado: %d\n", ft_count_symbols(teste));
+
+
+	char *teste2 = ">> OLA MUNDO >>< Vai dar erro aqui";
+	printf("Deveria ser -1 Resultado: %d\n", ft_count_symbols(teste2));
+
+
+	char *teste3 = "cat main.c > file1.txt | > outfile.txt";
+	printf("Deveria ser 2 Resultado: %d\n", ft_count_symbols(teste3));
+
+
+	char *teste4 = "cat main.c << heredoc >> file1.txt >> file2.txt";
+	printf("Deveria ser 3 Resultado: %d\n", ft_count_symbols(teste4));
+
+
+	char *teste5 = "cat main.c > < algo aqui >> OREMOS";
+	printf("Deveria ser -1 Resultado: %d\n", ft_count_symbols(teste5));
+
+
+	char *teste6 = "<file1.txt>>mais>>um>teste>>infernal";
+	printf("Deveria ser 5 Resultado: %d\n", ft_count_symbols(teste6));
+
+	//ft_prompt(blk, inp);
 	ft_freeing(blk->envp);
 	//ft_freeing(teste2);
 	free(blk);
