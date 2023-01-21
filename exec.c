4849a7d819587c291510e0c158904fc62a33dadc
jeluiz4 <jeffluiz97@gmail.com>
2023-01-21 09:34:51 -0300

    Args_Like_Argv_Done

--- exec.c
+++ exec.c
@@ -6,7 +6,7 @@
 /*   By: jeluiz4 <jeffluiz97@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 08:31:12 by jeluiz4           #+#    #+#             */
-/*   Updated: 2023/01/06 10:37:54 by jeluiz4          ###   ########.fr       */
+/*   Updated: 2023/01/21 09:24:35 by jeluiz4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,14 +52,16 @@ void	ft_access(t_shell *blk, t_input *inp)
 			free(inp->cmd);
 		inp->cmd = inp->args[0];
 		blk->rs = 0;
-		printf("ABSOLUTE PATH SUPREMACY\n");
 		ft_exec(inp, blk);
+		return ;
 	}
-	else if (inp->cmd != NULL)
+	else if ((inp->cmd != NULL) && (inp->args[0][0] != '\0') && (inp->args[0][0] != ' '))
 	{
 		ft_build_exec(blk, inp);
+		if (blk->rs == 0)
+			return ;
 	}
-	if ((inp->cmd != NULL) & (blk->rs == 1))
+	if (blk->rs == 1 || (inp->args[0][0] != '\0') || (inp->args[0][0] != ' '))
 	{
 		perror("PATH NOT FOUND");
 		blk->rs = 1;
