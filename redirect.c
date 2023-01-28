d9111ecd844075ef002dd13c14cf0bcb2998593f
jeluiz4 <jeffluiz97@gmail.com>
2023-01-28 13:23:24 -0300

    File_Open_Fix

--- redirect.c
+++ redirect.c
@@ -6,33 +6,41 @@
 /*   By: dvargas <dvargas@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:54:43 by dvargas           #+#    #+#             */
-/*   Updated: 2023/01/27 11:32:13 by jeluiz4          ###   ########.fr       */
+/*   Updated: 2023/01/28 13:22:29 by jeluiz4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_mini.h"
 #include "libft/libft.h"
 
-void	ft_heredoc_open(t_shell *blk, char *str)
+int	ft_heredoc_open(t_shell *blk, char *str)
 {
 	ft_heredoc(blk, str);
 	blk->fd_in = open(blk->tmpdoc, O_RDONLY);
 	if (blk->fd_in < 0)
+	{
 		perror("ERNANI");
+		return (0);
+	}
 	close(0);
 	dup2(blk->fd_in, 0);
+	return (1);
 }
 
-void	ft_infile_open(t_shell *blk, char *str)
+int	ft_infile_open(t_shell *blk, char *str)
 {
 	blk->fd_in = open(str, O_RDONLY);
 	if (blk->fd_in < 0)
+	{
 		perror("ERNANI");
+		return (0);
+	}
 	close(0);
 	dup2(blk->fd_in, 0);
+	return (1);
 }
 
-void	ft_outfile_open(char *str, int flag, t_shell *blk)
+int	ft_outfile_open(char *str, int flag, t_shell *blk)
 {
 	char	*outfile_name;
 	int		i;
@@ -47,45 +55,52 @@ void	ft_outfile_open(char *str, int flag, t_shell *blk)
 		blk->fd_in = open(outfile_name, O_TRUNC | O_CREAT | O_WRONLY, 0777);
 	free(outfile_name);
 	dup2(blk->fd_in, 1);
+	return (1);
 }
 
-void	ft_split_inf(t_shell *blk, char **tmp, int j)
+int	ft_split_inf(t_shell *blk, char **tmp, int j)
 {
 	char	**aux;
+	int		out;
 
 	aux = ft_split(tmp [j + 1], ' ');
-	ft_infile_open(blk, aux[0]);
+	out = ft_infile_open(blk, aux[0]);
 	ft_freeing(aux);
+	return (out);
 }
 
-void	ft_split_hdoc(t_shell *blk, char **tmp, int j)
+int	ft_split_hdoc(t_shell *blk, char **tmp, int j)
 {
 	char	**aux;
+	int		out;
 
 	aux = ft_split(tmp [j + 1], ' ');
-	ft_heredoc_open(blk, aux[0]);
+	out = ft_heredoc_open(blk, aux[0]);
 	ft_freeing(aux);
+	return (out);
 }
 
-void	ft_redirect_do(t_shell *blk, char **tmp, char *basestring, int j)
+int	ft_redirect_do(t_shell *blk, char **tmp, char *basestring, int j)
 {
 	int		i;
+	int		out;
 
 	i = -1;
 	while (basestring[++i])
 	{
 		if (basestring[i] == SHIFT_R)
-			ft_outfile_open(tmp[j + 1], 42, blk);
+			out = ft_outfile_open(tmp[j + 1], 42, blk);
 		if (basestring[i] == SHIFT_L)
-			ft_split_inf(blk, tmp, j);
+			out = ft_split_inf(blk, tmp, j);
 		if (basestring[i] == SHIFT_DR)
-			ft_outfile_open(tmp[j + 1], 1, blk);
+			out = ft_outfile_open(tmp[j + 1], 1, blk);
 		if (basestring[i] == SHIFT_DL)
-			ft_split_hdoc(blk, tmp, j);
+			out = ft_split_hdoc(blk, tmp, j);
 		if (!basestring[i + 1])
 			close(blk->fd_in);
 		j++;
 	}
+	return (out);
 }
 
 void	ft_redirect_access(t_shell *blk, t_input *inp, char **redir)
@@ -104,12 +119,13 @@ void	ft_simple_redirect(t_shell *blk, t_input *inp, char **splited)
 {
 	char	**tmp;
 	char	*basestring;
+	int		flag;
 
 	basestring = ft_red_stk(blk->buf);
 	tmp = ft_compose_cmd(splited);
-	ft_redirect_do(blk, splited, basestring, 0);
+	flag = ft_redirect_do(blk, splited, basestring, 0);
 	ft_freeing(splited);
-	if (tmp[0])
+	if (tmp[0] && flag)
 		ft_redirect_access(blk, inp, tmp);
 	ft_restore_fds(blk);
 	ft_freeing(tmp);
