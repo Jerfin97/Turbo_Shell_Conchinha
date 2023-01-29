b79a034672f55becd9200f856e920de75fb23b5d
jeluiz4 <jeffluiz97@gmail.com>
2023-01-29 11:25:02 -0300

    Heredoc_Freed

--- heredoc.c
+++ heredoc.c
@@ -6,7 +6,7 @@
 /*   By: dvargas <dvargas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 09:05:37 by dvargas           #+#    #+#             */
-/*   Updated: 2023/01/29 09:06:53 by dvargas          ###   ########.fr       */
+/*   Updated: 2023/01/29 11:24:29 by jeluiz4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,13 +53,11 @@ char	*ft_here_routine(char *hereword)
 }
 */
 
-char	*ft_here_routine(char *hereword)
+char	*ft_here_routine(char *hereword, char *aux)
 {
 	char	*buffer;
-	char	*aux;
 
 	aux = ft_calloc(1, 1);
-	g_return = -42;
 	while (42)
 	{
 		buffer = readline("heredoc> ");
@@ -67,7 +65,7 @@ char	*ft_here_routine(char *hereword)
 		{
 			free(buffer);
 			if (g_return == 130)
-				return (NULL);
+				return (free(aux), NULL);
 			printf("Conchinha: warning: here-document delimited by");
 			printf(" end-of-file (wanted `%s')\n", hereword);
 			break ;
@@ -78,8 +76,7 @@ char	*ft_here_routine(char *hereword)
 		ft_swapjoin(&aux, "\n");
 		free(buffer);
 	}
-	free(buffer);
-	return (aux);
+	return (free(buffer), aux);
 }
 
 void	ft_heredoc(t_shell *blk, char *hereword)
@@ -87,9 +84,12 @@ void	ft_heredoc(t_shell *blk, char *hereword)
 	int		fd;
 	char	*buffer;
 	char	*tmp;
+	char	*aux;
 
 	fd = open(blk->tmpdoc, O_TRUNC | O_CREAT | O_WRONLY, 0777);
-	tmp = ft_here_routine(hereword);
+	g_return = -42;
+	aux = NULL;
+	tmp = ft_here_routine(hereword, aux);
 	if (tmp == NULL && g_return == 130)
 	{
 		close(fd);
