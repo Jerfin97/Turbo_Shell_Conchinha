f31a9e5e848ea55b90c2bfe6959f2fcc0e0ab5a8
jeluiz4 <jeffluiz97@gmail.com>
2023-01-29 18:43:44 -0300

    Heredoc_Working_In_Pipes

--- redirect.c
+++ redirect.c
@@ -6,12 +6,24 @@
 /*   By: dvargas <dvargas@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:54:43 by dvargas           #+#    #+#             */
-/*   Updated: 2023/01/29 08:43:16 by dvargas          ###   ########.fr       */
+/*   Updated: 2023/01/29 18:43:25 by jeluiz4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib_mini.h"
 #include "libft/libft.h"
+
+int	ft_state(t_shell *blk, char **tmp, int j)
+{
+	int	out;
+	int	fd_out;
+
+	fd_out = dup(1);
+	ft_restore_fds(blk);
+	out = ft_split_hdoc(blk, tmp, j);
+	dup2(fd_out, 1);
+	return (out);
+}
 
 int	ft_redirect_do(t_shell *blk, char **tmp, char *basestring, int j)
 {
@@ -28,7 +40,7 @@ int	ft_redirect_do(t_shell *blk, char **tmp, char *basestring, int j)
 		else if (basestring[i] == SHIFT_DR)
 			out = ft_outfile_open(tmp, j, 1, blk);
 		else if (basestring[i] == SHIFT_DL)
-			out = ft_split_hdoc(blk, tmp, j);
+			ft_state(blk, tmp, j);
 		if (!basestring[i + 1])
 			close(blk->fd_in);
 		j++;
